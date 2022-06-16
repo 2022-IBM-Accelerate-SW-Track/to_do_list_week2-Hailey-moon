05037c2aada8cf764a92b17f7ac26e32979e51d2
Hailey Moon <82325205+Hailey-moon@users.noreply.github.com>
2022-06-15 22:09:29 -0400

    added styling for about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,32 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import Hailey_Moon_Headshot from "../assets/Hailey_Moon_Headshot.jpg";  
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
         <div class="split left">
           <div className="centered">
-            <img
+            <img 
               className="profile_image"
-              src={profile_pic}
+              src={ Hailey_Moon_Headshot }
               alt="Profile Pic"
             ></img>
           </div>
         </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
-            </div>
+      <div className="split right">
+        <div className="centered">
+          <div className="name_title">Hailey Moon</div>
+          <div className="brief_description">
+            <p>IBM Accelerate Participant <br/> 
+            UI/UX Designer <br/> 
+            Avid Rock Climber <br/>
+            What's Next...</p>
           </div>
         </div>
       </div>
-    );
+    </div>
+    )
   }
 }
